68921e4716a1ca2fd701ad33f1f7c40e978e1825
Iryna Kolomoiec <kolomoyka@gmail.com>
2024-03-29 14:32:10 +0100

    update styles

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -2,15 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
-  width: 550px;
-  height: 750px;
+  max-width: 550px;
   background: transparent;
   border: 2px solid;
   border-color: rgba(255, 255, 255, 0.5);
   border-radius: 20px;
   backdrop-filter: blur(10px);
   box-shadow: 0 0 30px rgba(0, 0, 0, 0.5);
-  margin: 0 auto;
+  margin: 20% auto;
 
   @media (max-width: 768px) {
   max-width: 300px;
